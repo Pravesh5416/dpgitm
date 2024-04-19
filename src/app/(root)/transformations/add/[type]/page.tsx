9cc2221ae94da4f6ba1ecb6f730224e59b7a2982
Pravesh <kumarpravesh3867@gmail.com>
2024-04-19 14:57:14 +0530

    'commit'

--- src/app/(root)/transformations/add/[type]/page.tsx
+++ src/app/(root)/transformations/add/[type]/page.tsx
@@ -21,11 +21,15 @@ const AddTransformationTypePage = async ({ params: { type } }: SearchParamProps)
         title={transformation.title}
         subtitle={transformation.subTitle}
       />
-      <TransformationForm action="Add"
+
+      <section className="mt-10">
+        <TransformationForm action="Add"
         userId={user._id}
         type={transformation.type as TransformationTypeKey}
         creditBalance={user.creditBalance}
       />
+      </section>
+      
     </>
 
   )
